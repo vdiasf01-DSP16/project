adb79db9a81f4cedc7ca0fbebf5067bbea8ab94a
Vasco Horta <diasfonseca@live.com>
2016-05-06 19:34:56 +0100

    DeNormalise
    Removing not needed package import

--- NeuralNetworkTrainingTool/src/test/app/model/dataSet/TestDeNormalise.java
+++ NeuralNetworkTrainingTool/src/test/app/model/dataSet/TestDeNormalise.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import app.model.dataSet.DeNormalize;
-import app.model.dataSet.Normalize;
 
 /**
  * Testing the DeNormalise class implementation.
