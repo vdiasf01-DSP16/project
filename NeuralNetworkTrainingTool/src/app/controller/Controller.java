4c7f744f2d15da5dbd5f36d0d53a09090daa0e97
Vasco Horta <diasfonseca@live.com>
2016-06-16 21:11:08 +0100

    Main Controller
    The main controller interface adding initial required methods as per GUI

--- NeuralNetworkTrainingTool/src/app/controller/Controller.java
+++ NeuralNetworkTrainingTool/src/app/controller/Controller.java
@@ -1,6 +1,7 @@
 package app.controller;
 
 import app.model.project.ProjectData;
+import javafx.stage.Stage;
 
 /**
  * Main application controller between the view the the model.
@@ -9,12 +10,39 @@ import app.model.project.ProjectData;
  *
  */
 public interface Controller {
-	
+
+	/**
+	 * The application Stage.
+	 * 
+	 * @return Stage
+	 */
+	public Stage getStage();
+
+	/**
+	 * Setting the application Stage.
+	 * 
+	 * @return Stage
+	 */
+	public void setStage(Stage stage);
+
 	/**
 	 * Saves received project details.
 	 * 
 	 * @param projectDetails
 	 */
 	public void saveProject(ProjectData projectDetails);
+
+	/**
+	 * Checks if nothing is pending and it is ok to quit the application
+	 * without losing any important data.
+	 * 
+	 * @return true if ok.
+	 */
+	public boolean isOkToQuit();
+
+	/**
+	 * Save all parts to disk.
+	 */
+	public void saveAll();
 
 }
